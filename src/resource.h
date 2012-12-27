b1dd870174921b010549aaf74671506e3547d5e1
zth555 <zth555@qq.com>
2012-12-27 17:13:33 +0000

    增加对话框资源。 初步设计用户界面。

--- src/resource.h
+++ src/resource.h
@@ -2,14 +2,39 @@
 // Microsoft Developer Studio generated include file.
 // Used by AccountMaster.rc
 //
+#define IDD_LOGIN                       101
+#define IDD_REGISTER                    102
+#define IDD_FORGETPWD2                  104
+#define IDD_FORGETPWD1                  105
+#define IDD_FORGETPWD3                  106
+#define IDD_MAIN                        107
+#define IDM_MAIN                        108
+#define IDC_EDIT1                       1000
+#define IDC_EDIT2                       1001
+#define IDC_BUTTON1                     1002
+#define IDC_EDIT3                       1002
+#define IDC_BUTTON2                     1003
+#define IDC_EDIT4                       1003
+#define IDC_BUTTON3                     1004
+#define IDC_COMBO1                      1005
+#define IDC_BUTTON4                     1005
+#define IDC_LIST1                       1006
+#define ID_USER_SETTINGS                40001
+#define ID_USER_DEL                     40002
+#define ID_ACCOUNT_ADD                  40003
+#define ID_ACCOUNT_DEL                  40004
+#define ID_DB_1                         40005
+#define ID_DB_2                         40006
+#define ID_DB_BACKUP                    40007
+#define ID_DB_RESUME                    40008
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_RESOURCE_VALUE        109
+#define _APS_NEXT_COMMAND_VALUE         40009
+#define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
